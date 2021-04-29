0d0f93b43c7371a6be3f0fea341b1ff3490fa7b8
*Satyasri-Samayamanthula* <ibmjavafsdmr16@Javafsdnh260.iiht.tech>
2021-04-29 10:28:48 +0530

    SMS

---
+++ spring-boot-OTP-app/src/main/java/com/pixeltrice/springbootOTPapp/SmsService.java
@@ -0,0 +1,33 @@
+package com.pixeltrice.springbootOTPapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+
+@Component
+public class SmsService {
+
+	    
+	    private final String ACCOUNT_SID ="ACa108c6bd08281fe95b1529e2c480b3b3o";
+
+	    private final String AUTH_TOKEN = "1ec7b4c7a33e5ec39f74d655a834939a";
+
+	    private final String FROM_NUMBER = "+14694151561";
+
+	    public void send(SmsPojo sms) {
+	    	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+	        Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage())
+	                .create();
+	        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
+
+	    }
+
+	    public void receive(MultiValueMap<String, String> smscallback) {
+	    }
+	
+}
